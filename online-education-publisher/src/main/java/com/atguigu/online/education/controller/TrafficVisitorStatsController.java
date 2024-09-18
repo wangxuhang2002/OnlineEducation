cae4990a45c388d6fe8f9c7f232a3427cad7ab21
wangxuhang <wangxuhang2002@163.com>
2024-09-18 19:36:43 +0800

    测试-3

--- online-education-publisher/src/main/java/com/atguigu/online/education/controller/TrafficVisitorStatsController.java
+++ online-education-publisher/src/main/java/com/atguigu/online/education/controller/TrafficVisitorStatsController.java
@@ -53,11 +53,6 @@ public class TrafficVisitorStatsController {
             }
         }
 
-        System.out.println(StringUtils.join(hrs, "\",\""));
-        System.out.println(StringUtils.join(uvArr, "\",\""));
-        System.out.println(StringUtils.join(pvArr, "\",\""));
-        System.out.println(StringUtils.join(pvPerSession, "\",\""));
-
         return "{\n" +
                 "  \"status\": 0,\n" +
                 "  \"msg\": \"\",\n" +
